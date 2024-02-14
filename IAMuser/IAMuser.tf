0375a2e34127b55d12ea839fad358a8d235aef84
shubham <shubhamgorde@gmail.com>
2024-02-14 22:09:02 +0530

    --- Auto Git Commit ---

--- IAMuser/IAMuser.tf
+++ IAMuser/IAMuser.tf
@@ -8,4 +8,28 @@ resource "aws_iam_user" "my_user"{
     tags = {
     tag-key = "env-dev"
   }
-} 
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "${random_pet.pet_name.id}-policy"
+  description = "My test policy"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "AdministratorAccess-Amplify"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+}
+]
+}
+}
+
+resource "aws_iam_user_policy_attachment" "test-attach" {
+  user       = aws_iam_user.my_user.name
+  policy_arn = aws_iam_policy.policy.arn
+}
