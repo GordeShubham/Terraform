ed2ae1391064488362517ad3b6199ac294ee4aef
shubham <shubhamgorde@gmail.com>
2024-02-16 00:40:56 +0530

    --- Auto Git Commit ---

--- IAMuser/IAMuser.tf
+++ IAMuser/IAMuser.tf
@@ -23,3 +23,32 @@ resource "aws_s3_bucket_ownership_controls" "demo_bucket" {
   bucket = aws_s3_bucket.demo_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
+
+resource "aws_iam_user_policy" "s3_bucket_policy" {
+  name = "s3-bucket-policy"
+  user = aws_iam_user.s3_bucket_policy.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+           "s3:GetObject",
+           "s3:PutObject"
+
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user" "s3_bucket_policy" {
+  name = "bucket-policy"
+  path = "/"
+}
+
+resource "aws_iam_access_key" "s3_bucket_policy" {
+  user = aws_iam_user.s3_bucket_policy.name
+}
